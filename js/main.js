56a0476f51476d594d4265403947ece42c43285e
kenjimika77-design <kenjimika77@gmail.com>
2026-06-02 00:28:51 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+document.getElementById("enterBtn").addEventListener("click", () => {
+  document.body.style.opacity = "0";
 
+  setTimeout(() => {
+    window.location.href = "library.html";
+  }, 600);
+});
